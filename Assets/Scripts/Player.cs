44ad630d5d98b913c50498195c60f0e6bae7783c
rau-git <itsjordannisocialm@gmail.com>
2022-11-14 16:25:29 +0000

    basic movement system implemented

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    private PlayerIngameControls _playerIngameControls;
+    private Rigidbody _playerRigidbody;
+    [SerializeField] private float _playerMoveSpeed;
+    [SerializeField] private float _playerJumpPower;
+    private float _gravity;
+
+    private void Awake()
+    {
+        _playerIngameControls = new PlayerIngameControls();
+        _playerIngameControls.Enable();
+        _playerRigidbody = GetComponent<Rigidbody>();
+        _gravity = Physics.gravity.y;
+    }
+
+    private void OnEnable()
+    {
+        _playerIngameControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerIngameControls.Disable();
+    }
+
+    private void Update()
+    {
+        MovePlayer();
+        Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        BetterGravity();
+    }
+
+    private void MovePlayer()
+    {
+        Vector2 playerMovementInput = _playerIngameControls.PlayerControls.Movement.ReadValue<Vector2>();
+        Vector3 playerMovementDirection = new Vector3(playerMovementInput.x, 0, playerMovementInput.y);
+
+        _playerRigidbody.MovePosition(transform.position + playerMovementDirection * _playerMoveSpeed * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        if (_playerIngameControls.PlayerControls.Jump.triggered)
+        {
+            _playerRigidbody.AddForce(transform.up * _playerJumpPower, ForceMode.VelocityChange);
+        }
+    }
+
+    private void BetterGravity()
+    {
+        _playerRigidbody.AddForce(transform.up * _gravity, ForceMode.Acceleration);
+    }
+}
